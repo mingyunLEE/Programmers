bc7be52ce466c3e75a6db6cd9078f91213206f17
mingyunLEE <leemingyun@naver.com>
2023-03-02 23:11:29 +0900

    reverse string

--- Programmers/untitled/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String arr = "1,2,3,4,5,6,7,8,9";
-        String[] result = arr.split(",");
-        System.out.println(result[0]);
-}
-}
